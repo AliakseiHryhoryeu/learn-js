0bfbf7e37b1e3638035ea475bab1be1a0a171791
AliakseiHryhoryeu <88304337+AliakseiHryhoryeu@users.noreply.github.com>
2022-03-05 17:43:31 +0300

    create next-js folder

---
+++ next-js/pages/contacts/index.js
@@ -0,0 +1,8 @@
+const Contact = () => (
+    <>
+      <h1>All contacts</h1>
+    </>
+  );
+  
+  export default Contact;
+  
---
+++ next-js/pages/posts.js
@@ -0,0 +1,8 @@
+const Posts = () => (
+  <>
+    <h1>Posts page</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing.</p>
+  </>
+);
+
+export default Posts;
