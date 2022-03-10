89ab721f6c8cb1c5a192edd34f9576ef016a6158
AliakseiHryhoryeu <88304337+AliakseiHryhoryeu@users.noreply.github.com>
2022-03-10 14:50:06 +0300

    update index.tsx

--- next-js/pages/index.tsx
+++ next-js/pages/index.tsx
@@ -1,25 +1,33 @@
 import { FC } from "react"
-import {GetStaticProps} from "next"
+import { GetStaticProps } from "next"
 import Head from "next/head"
 import Heading from "../components/Heading"
 import styles from '../styles/Home.module.scss'
 import Socials from '../components/Socials'
 
-import config from '../config/config.json'
 import { socialsType } from "../types"
 
-export const getStaticProps:GetStaticProps = async () => {
-  const response = await fetch(config.serverIp + '/api/socials')
-  const data = await response.json()
+export const getStaticProps: GetStaticProps = async () => {
 
-  if (!data) {
-    return {
-      notFound: true
+  try {
+    const response = await fetch(`${process.env.API_HOST}/socials`)
+    const data = await response.json()
+
+    if (!data) {
+      return {
+        notFound: true
+      }
     }
+    return {
+      props: { socials: data }
+    }
+  } catch {
+    return {
+      props: { socials: null }
+    }
+
   }
-  return {
-    props: { socials: data }
-  }
+
 }
 type homeTypeProps = {
   socials: [socialsType]
