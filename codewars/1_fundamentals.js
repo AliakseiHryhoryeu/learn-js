633d6b87b803ca76689c3d2602314270d395b4cc
AliakseiHryhoryeu <88304337+AliakseiHryhoryeu@users.noreply.github.com>
2022-04-11 11:13:21 +0300

    Update 1_fundamentals.js

--- codewars/1_fundamentals.js
+++ codewars/1_fundamentals.js
@@ -129,3 +129,56 @@ function descendingOrder(number) {
 }
 
 // console.log(descendingOrder(15)) //
+
+// Write a function that returns both the minimum and maximum number of the given list/array.
+
+function minMax(arr) {
+  let min = arr[0]
+  let max = arr[0]
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > max) {
+      max = arr[i]
+    }
+    if (arr[i] < min) {
+      min = arr[i]
+    }
+  }
+  return [min, max]
+}
+
+// console.log(minMax([1, 2, 3, 4, 5])) // [1, 5]
+
+// There is a bus moving in the city, and it takes and drop some people in each bus stop.
+// You are provided with a list (or array) of integer pairs. Elements of each pair represent number of people get into bus (The first item) and number of people get off the bus (The second item) in a bus stop.
+// Your task is to return number of people who are still in the bus after the last bus station (after the last array). Even though it is the last bus stop, the bus is not empty and some people are still in the bus, and they are probably sleeping there :D
+// Take a look on the test cases.
+// Please keep in mind that the test cases ensure that the number of people in the bus is always >= 0. So the return integer can't be negative.
+// The second value in the first integer array is 0, since the bus is empty in the first bus stop.
+
+
+let number = function (busStops) {
+  let people = 0;
+  busStops.forEach(a => people = people + a[0] - a[1]);
+  return people
+}
+
+// console.log(number([[3,0],[9,1],[4,10],[12,2],[6,1],[7,10]])) // 17
+
+
+// This time no story, no theory. The examples below show you how to write function accum:
+// 
+// Examples:
+// accum("abcd") -> "A-Bb-Ccc-Dddd"
+// accum("RqaEzty") -> "R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy"
+// accum("cwAt") -> "C-Ww-Aaa-Tttt
+
+function accum(s) {
+  var letters = str.split('');
+  var result = [];
+  for (var i = 0; i < letters.length; i++) {
+    result.push(letters[i].toUpperCase() + Array(i + 1).join(letters[i].toLowerCase()));
+  }
+  return result.join('-');
+}
+
+console.log(accum("ZpglnRxqenU")) // "Z-Pp-Ggg-Llll-Nnnnn-Rrrrrr-Xxxxxxx-Qqqqqqqq-Eeeeeeeee-Nnnnnnnnnn-Uuuuuuuuuuu"
