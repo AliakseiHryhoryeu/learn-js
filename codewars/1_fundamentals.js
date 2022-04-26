0431cc48dbf958a58423a79b7bcc0b0fdb1243d0
Aliaksei Hryhoryeu <88304337+AliakseiHryhoryeu@users.noreply.github.com>
2022-04-26 16:30:57 +0300

    Update 1_fundamentals.js

--- codewars/1_fundamentals.js
+++ codewars/1_fundamentals.js
@@ -230,5 +230,20 @@ function smallEnough(a, limit) {
 // console.log(smallEnough([78, 117, 110, 99, 104, 117, 107, 115], 100)) // false
 
 
+// Trolls are attacking your comment section!
+// A common way to deal with this situation is to remove all of the vowels from the trolls' comments, neutralizing the threat.
+// Your task is to write a function that takes a string and return a new string with all vowels removed.
+// For example, the string "This website is for losers LOL!" would become "Ths wbst s fr lsrs LL!".
+// Note: for this kata y isn't considered a vowel.
 
+const disemvowel =(str)=> {
+  let newStr = ""
+  for (let i = 0; i < str.length; i++) {
+      if ("aeiou".includes(str[i].toLowerCase())) continue;
+      newStr += str[i];
+  }
+  return newStr;
+}
+
+console.log(disemvowel("This website is for losers LOL!")) //  "Ths wbst s fr lsrs LL!"
 
